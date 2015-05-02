1e49faa9d8a09fbab400f7b09d53f0aaf7dff2c4
Treer <treer.git@the-bordello.com>
2015-05-03 03:18:22 +1000

    Accuracy of structures improved.
    Accuracy improvement part 3:
     * Eliminates 18% of all villages, with a false negative rate below 2% (40 villages elimated out of 223, about 30 of those villages were checked and 2 of those were found to exist in the game)

--- src/amidst/map/layers/VillageLayer.java
+++ src/amidst/map/layers/VillageLayer.java
@@ -70,9 +70,69 @@ public class VillageLayer extends IconLayer {
 		i1 += random.nextInt(maxDistanceBetweenScatteredFeatures - minDistanceBetweenScatteredFeatures);
 		chunkX = k;
 		chunkY = m;
-		if ((chunkX == n) && (chunkY == i1))
-			return MinecraftUtil.isValidBiome(chunkX * 16 + 8, chunkY * 16 + 8, structureSize, validBiomes);
-		
+		if ((chunkX == n) && (chunkY == i1)) {
+			// This is a potential feature biome			
+			
+			int chunkCenterBlockX = chunkX * 16 + 8;
+			int chunkCenterBlockY = chunkY * 16 + 8;
+			
+			boolean canSpawnStructureAtCoords = MinecraftUtil.isValidBiome(chunkCenterBlockX, chunkCenterBlockY, structureSize, validBiomes);
+			
+			if (canSpawnStructureAtCoords) {
+				// Villages start will a well, size 6 x 6, extending to the right and down from 
+				// the village spawn coordinates. The well starts the village bounding box at 6 x 6.
+				//
+				// My hypothesis was as follows:
+				//   "If a river or non-valid biome cuts through this area then none of the paths extending from
+				//   the well will generate (as I think they each retest the bounding box against valid biomes as they 
+				//   extend it). If none of the paths generate then I think the Village fails as a certain number
+				//   of buildings are required - it even looks like more than 2 roads may be required."
+				//
+				// Village at [-5008, -8000] and [-6352, -8144] on seed -1364077613 disprove the hypothesis, yet 
+				// strangely it still appears to be a fantastic improvement to village accuracy - eliminating 18% of 
+				// all villages from the 20km test area with those two being the only false negative I've found (having 
+				// checked half of the eliminated villages). So I will keep this code in AmidstExporter, but leave 
+				// it up to other maintainers to decide if it goes in their versions.
+				//
+				// The village paths must be only biome-testing their own bounding box, rather than the villages, so 
+				// beats me why this works so well, just a strong tendency of villages near boundaries to fail I guess.
+				int wellSize = 6;		
+				int x1 = chunkX * 16 + 2; // For some reason MapGenVillage.Start.Start() adds only 2 to the multiplied coord
+				int y1 = chunkY * 16 + 2;
+				int x2 = x1 + wellSize - 1; 
+				int y2 = y1 + wellSize - 1;
+				int wellX = (x1 + x2) / 2;
+				int wellY = (y1 + y2) / 2;
+				// It looked like there was an arbitraryConstant of 4 in the Minecraft source that's added to bounding 
+				// box sizes to get the "structureSize" for areBiomesViable() (e.g. see func_176069_e()), but using 4 
+				// eliminates several villages confirmed to exist. The source is hard to read and I've obviously missed a 
+				// lot that's going on, so trying a different tact... 
+				// Setting arbitraryConstant to 1 means a wellStructureSize of 3 -> the area 
+				// passed to isValidBiome() will exactly match the visual footprint of the well. Setting arbitraryConstant 
+				// to 2 will mean a wellStructureSize of 4 which corresponds to a 1-block lip around the well also being
+				// checked for bad biomes (at 1/4th the block resolution, too). 
+				// Testing all villages within 20km of the seed -1364077613, I see that the difference between arbitraryConstant 
+				// of 1 vs 2 is that a value of 2 correctly eliminates a further 8 villages and none of those 8 villages exist 
+				// in the game, however values above 2 will eliminate more villages that do exist (e.g. [2672, -5424] and 
+				// [3376, -3504]).
+				// So in lieu of a larger data set, lets set arbitraryConstant to 2 ;)
+				int arbitraryConstant = 0;
+				int wellStructureSize = (x2 - x1) / 2 + arbitraryConstant;
+							
+				boolean canSpawnWellAtCoords = MinecraftUtil.isValidBiome(wellX, wellY, wellStructureSize, validBiomes);
+				
+				// Checking that the well is able to build eliminates most of the false positives, however
+				// a few remain. Here are some villages unable to build sufficiently beyond the
+				// well (i.e. I have confirmed they do not exist in the game), which are not eliminated by
+				// canSpawnWellAtCoords. Found during testing on seed -1364077613:
+				//  *  2912,  2080
+				//  * -5888,  4784
+				//  * -7952, -9424
+				//  *  -272, -7872
+				return canSpawnWellAtCoords;
+			}
+		}
+	
 		return false;
 	}
 }
