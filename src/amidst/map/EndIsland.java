14828092b7d7e7f776fbce6c74470f256589406e
Treer <treer.git@the-bordello.com>
2015-11-14 17:47:32 +1100

    Adapt SimplexNoise.java to run off PRNG

--- src/amidst/map/EndIsland.java
+++ src/amidst/map/EndIsland.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import amidst.Options;
-import amidst.minecraft.Simplex2dNoise;
+import amidst.minecraft.SimplexNoise;
 
 public class EndIsland {
 	
@@ -14,11 +14,11 @@ public class EndIsland {
 	
 	public static final double cIslandDensityThreshold = -0.8999999761581421;
 	
-	private static Simplex2dNoise _cachedNoiseFunction = null;
+	private static SimplexNoise _cachedNoiseFunction = null;
 	private static long _cachedNoiseFunctionSeed;
 	
 	/** Returns the noise function using the current seed */
-	protected static Simplex2dNoise NoiseFunction() {
+	protected static SimplexNoise NoiseFunction() {
 		
 		if (_cachedNoiseFunctionSeed != Options.instance.seed || _cachedNoiseFunction == null) {
 			
@@ -30,7 +30,7 @@ public class EndIsland {
 			Perlin3dOctaves_FakeConstructor(random, 8);
 			Perlin3dOctaves_FakeConstructor(random, 10);
 			Perlin3dOctaves_FakeConstructor(random, 16);			
-			_cachedNoiseFunction = new Simplex2dNoise(random);
+			_cachedNoiseFunction = new SimplexNoise(random);
 		}
 		return _cachedNoiseFunction;
 	}
@@ -63,7 +63,7 @@ public class EndIsland {
 	}
 	
 	/** Returns an EndIsland if one has 'grown out' from the chunk, otherwise null */
-	protected static EndIsland checkForIsland(int chunkX, int chunkZ, Simplex2dNoise noiseFunction) {
+	protected static EndIsland checkForIsland(int chunkX, int chunkZ, SimplexNoise noiseFunction) {
 		
 		EndIsland result = null;
 		
@@ -74,7 +74,7 @@ public class EndIsland {
 		} else if (chunkX * chunkX + chunkZ * chunkZ > 4096) {
 			// The chunk is in the outer-islands band (1024 blocks from the origin)
 						
-			if (noiseFunction.a(chunkX, chunkZ) < cIslandDensityThreshold) {
+			if (noiseFunction.noise(chunkX, chunkZ) < cIslandDensityThreshold) {
 				// An island (or part of an island) grows out from this chunk,
 				// with an erosion factor between 9 and 21 (i.e. they will be 
 				// smaller than the main island).
@@ -94,7 +94,7 @@ public class EndIsland {
 	
 		List<EndIsland> result = new ArrayList<EndIsland>();
 		
-		Simplex2dNoise noiseFunction = NoiseFunction();
+		SimplexNoise noiseFunction = NoiseFunction();
 		
 		// Minecraft checks 12 chunks either side of a chunk when assessing
 		// island influence. 
--- src/amidst/minecraft/SimplexNoise.java
+++ src/amidst/minecraft/SimplexNoise.java
@@ -1,6 +1,9 @@
 package amidst.minecraft;
+//This file has been adapted for use in AmidstExporter, the 
 //original SimplexNoise code and related documents can be found at:
 //http://webstaff.itn.liu.se/~stegu/simplexnoise/
+
+import java.util.Random;
 
 
 /*
@@ -34,29 +37,9 @@ public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
                    new Grad(1,1,1,0),new Grad(1,1,-1,0),new Grad(1,-1,1,0),new Grad(1,-1,-1,0),
                    new Grad(-1,1,1,0),new Grad(-1,1,-1,0),new Grad(-1,-1,1,0),new Grad(-1,-1,-1,0)};
 
-  private static short p[] = {151,160,137,91,90,15,
-  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
   // To remove the need for index wrapping, double the permutation table length
-  private static short perm[] = new short[512];
-  private static short permMod12[] = new short[512];
-  static {
-    for(int i=0; i<512; i++)
-    {
-      perm[i]=p[i & 255];
-      permMod12[i] = (short)(perm[i] % 12);
-    }
-  }
+  private short perm[];
+  private short permMod12[];
 
   // Skewing and unskewing factors for 2, 3, and 4 dimensions
   private static final double F2 = 0.5*(Math.sqrt(3.0)-1.0);
@@ -66,6 +49,35 @@ public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
   private static final double F4 = (Math.sqrt(5.0)-1.0)/4.0;
   private static final double G4 = (5.0-Math.sqrt(5.0))/20.0;
 
+  
+  public SimplexNoise(Random rand) {
+	  
+	  // To remove the need for index wrapping, double the permutation table length
+      perm      = new short[512];
+      permMod12 = new short[512];
+	  
+	  // Minecraft uses the PRNG 3 times before building the 
+	  // permutation table.
+	  rand.nextDouble();
+	  rand.nextDouble();
+	  rand.nextDouble();
+	  	  
+      // Build a permutation table using our seeded PRNG
+      for (short i = 0; i < 256; ++i) {
+          perm[i] = i;
+      }
+      for (int i = 0; i < 256; ++i) {
+          final int swapIndex = rand.nextInt(256 - i) + i;
+          final short temp = perm[i];
+          perm[i] = perm[swapIndex];
+          perm[swapIndex] = temp;
+          perm[i + 256] = perm[i];
+          
+          permMod12[i] = permMod12[i + 256] = (short)(perm[i] % 12);
+      }
+  }
+  
+  
   // This method is a *lot* faster than using (int)Math.floor(x)
   private static int fastfloor(double x) {
     int xi = (int)x;
@@ -83,7 +95,7 @@ public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
 
 
   // 2D simplex noise
-  public static double noise(double xin, double yin) {
+  public double noise(double xin, double yin) {
     double n0, n1, n2; // Noise contributions from the three corners
     // Skew the input space to determine which simplex cell we're in
     double s = (xin+yin)*F2; // Hairy factor for 2D
@@ -138,7 +150,7 @@ public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
 
 
   // 3D simplex noise
-  public static double noise(double xin, double yin, double zin) {
+  public double noise(double xin, double yin, double zin) {
     double n0, n1, n2, n3; // Noise contributions from the four corners
     // Skew the input space to determine which simplex cell we're in
     double s = (xin+yin+zin)*F3; // Very nice and simple skew factor for 3D
@@ -220,7 +232,7 @@ public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
 
 
   // 4D simplex noise, better simplex rank ordering method 2012-03-09
-  public static double noise(double x, double y, double z, double w) {
+  public double noise(double x, double y, double z, double w) {
 
     double n0, n1, n2, n3, n4; // Noise contributions from the five corners
     // Skew the (x,y,z,w) space to determine which cell of 24 simplices we're in
