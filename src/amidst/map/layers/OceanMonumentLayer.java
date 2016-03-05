0d35e1bd2bd534503e01ca1c942d0be22daec95a
Treer <treer.git@the-bordello.com>
2016-03-05 16:25:13 +1100

    Update Ocean Monuments to reflect changes in 15w46a Bug https://bugs.mojang.com/browse/MC-65214 was fixed in 15w46a, and that fix changed where Ocean Monuments can appear.

--- src/amidst/map/layers/OceanMonumentLayer.java
+++ src/amidst/map/layers/OceanMonumentLayer.java
@@ -30,6 +30,7 @@ public class OceanMonumentLayer extends IconLayer {
 	 * https://www.reddit.com/r/Minecraft/comments/28sbtm/a_minecraft_experiment_just_how_does_the_new_18/cidzxpi?context=1
 	 */
 	public static final boolean cAllVersionsCanShowOceanMonuments = true;
+	private boolean useFixedAlogirithm = true;	
 	
 	public static List<Biome> validBiomes = Arrays.asList(
 		new Biome[] { 
@@ -44,7 +45,14 @@ public class OceanMonumentLayer extends IconLayer {
 				Biome.frozenOcean,
 				Biome.river,
 				Biome.frozenRiver,
-				// Not sure if the extended biomes count
+				// Not sure if the extended biomes count. I think they do because Minecraft has
+				// a MutatedBiome class (or ModifiedBiome - I don't know what it's called in 
+				// Mojang's code) that defers equality checks to its base biome, and biome list
+				// checks in Minecraft are done with List<>.contains(), which will use that
+				// base-biome equality check. But this stuff has been refactored a lot so it's
+				// hard to know what the story is. 
+				// Right now it doesn't matter because these mutated biomes are never created 
+				// in the game.
 				Biome.oceanM, 
 				Biome.deepOceanM, 
 				Biome.frozenOceanM,
@@ -56,6 +64,51 @@ public class OceanMonumentLayer extends IconLayer {
 	private Random random = new Random();
 	
 	public OceanMonumentLayer() {
+		
+		if (MinecraftUtil.getVersion().isAtLeast(VersionInfo.V15w46a)) {			
+			// Use the current ocean monuments algorithm
+			
+			// Bug https://bugs.mojang.com/browse/MC-65214 was fixed in 15w46a,
+			// and the fix changes where Ocean Monuments can appear.
+			// This class implements the LocationChecker for after the fix was 
+			// implemented.
+			//  
+			// The fix is described here: https://bugs.mojang.com/browse/MC-65214?focusedCommentId=228462&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-228462
+			// That description is quoted below:
+			// 
+			// ----
+			//    "The issue lies in that the server is calculating DEEP_BIOME based on 
+			//    the 1.8 World Generator and NOT the worlds ACTUAL biome from previous 
+			//    versions.
+			//
+			//    I had a report of a biome in OCEAN (Not Deep), and when I generated a 
+			//    1.8 world with exact same seed, sure enough that location was 
+			//    DEEP_OCEAN in that seed.
+			//
+			//    The Monument Structure check uses 2 different Biome lookup methods, and
+			//    the one that does DEEP_BIOME check uses the World Gen calculations, and
+			//    then the followup check for "Surrounding biomes" uses the actual worlds 
+			//    data.
+			//
+			//    I temp fixed for my server with the following change:
+			//        - if (this.c.getWorldChunkManager().getBiome(new BlockPosition(k * 16 + 8, 64, l * 16 + 8), (BiomeBase) null) != BiomeBase.DEEP_OCEAN) {
+			//        + if (!this.c.getWorldChunkManager().a(k * 16 + 8, l * 16 + 8, 16, Arrays.asList(BiomeBase.DEEP_OCEAN))) {
+			//        
+			//    This issue has more flaws than this report states as it also causes 
+			//    monuments to spawn in the middle of Rivers, Frozen Rivers and Frozen 
+			//    Oceans, which is quite odd to stroll through the mountains and find 
+			//    a monument."
+			// ----
+			// 
+			// Examples:
+			// In seed -3189899870270394863, the monuments at (808, 1224) and (-856, 184)
+			// are eliminated by this fix.			
+			useFixedAlogirithm = true;
+		} else {
+			useFixedAlogirithm = false;
+		}
+		
+		
 	}
 	
 	/**
@@ -125,6 +178,7 @@ public class OceanMonumentLayer extends IconLayer {
 		byte maxDistanceBetweenScatteredFeatures = 32;
 		byte minDistanceBetweenScatteredFeatures = 5;
 		int structureSize = 29;
+		int structureCenterSize = 16;
 		int structureMagicNumber = 10387313; // 10387313 is the magic salt for ocean monuments
 		
 		int chunkXadj = chunkX;
@@ -145,11 +199,20 @@ public class OceanMonumentLayer extends IconLayer {
 		
 		if ((chunkX == a) && (chunkY == b)) {
 
-			// Note that getBiomeAt() is full-resolution biome data, while isValidBiome() is calculated using
-			// quarter-resolution biome data. This is identical to how Minecraft calculates monuments.
-			result = 
-				MinecraftUtil.getBiomeAt(  chunkX * 16 + 8, chunkY * 16 + 8) == Biome.deepOcean &&
-				MinecraftUtil.isValidBiome(chunkX * 16 + 8, chunkY * 16 + 8, structureSize, validSurroundingBiomes); 
+			if (useFixedAlogirithm) {
+				result = 
+					MinecraftUtil.isValidBiome(chunkX * 16 + 8, chunkY * 16 + 8, structureCenterSize, validBiomes) &&
+					MinecraftUtil.isValidBiome(chunkX * 16 + 8, chunkY * 16 + 8, structureSize, validSurroundingBiomes);
+			} else {
+				// Note that getBiomeAt() is full-resolution biome data, while isValidBiome() is calculated using
+				// quarter-resolution biome data. This is identical to how Minecraft calculates monuments.
+				// I think getBiomeAt() is also using the world generatOR, whereas isValidBiome() is
+				// using the biomes of the generatED world - these can differ if the world was generated in an
+				// earlier version to what the client is currently running.
+				result = 
+					MinecraftUtil.getBiomeAt(  chunkX * 16 + 8, chunkY * 16 + 8) == Biome.deepOcean &&
+					MinecraftUtil.isValidBiome(chunkX * 16 + 8, chunkY * 16 + 8, structureSize, validSurroundingBiomes);
+			}
 		}
 		return result;
 	}
